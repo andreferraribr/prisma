fa71dba46f05fe7343d32c5084659270f9674372
andreferraribr <andre.ferrari.br@gmail.com>
2019-11-22 09:33:13 -0300

    posição texto gráfico filtrar investimento

--- App/app.R
+++ App/app.R
@@ -34,31 +34,26 @@ ui <- fluidPage(
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(height =  50,
+        sidebarPanel(height =  "50%",
             
             # Input: Simple integer interval ----
-            sliderInput("ano_loa", "ano do empenho:",
+            sliderInput("ano_loa", "ano do empenho/autorização do investimento:",
                         min = min(prisma$ano_lei), max = max(prisma$ano_lei),
                         value = c(min(prisma$ano_lei), max = max(prisma$ano_lei)),
                         step = 1),
             
             
             # Input: Decimal interval with step value ----
-            sliderInput("ano_pgt", "ano do pagamento:",
+            sliderInput("ano_pgt", "ano do pagamento do investimento:",
                         min = min(prisma$ano_despesa), max = max(prisma$ano_despesa),
                         value = c(min(prisma$ano_despesa), max = max(prisma$ano_despesa)),
-                        step = 1),
-            
-            # Input: Decimal interval with step value ----
-            selectInput ("gnd", label = "grupo da despesa",
-                         choices = unique(prisma$gnd),
-                         selected = "INVESTIMENTOS" )
+                        step = 1)
             
         ),
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("ano_lei", height =  200),
-           plotOutput("ano_pgt", height =  200)
+           plotOutput("ano_lei", height =  180),
+           plotOutput("ano_pgt", height =  180)
         )
     )
 )
@@ -72,14 +67,11 @@ server <- function(input, output) {
       prisma_lei<- prisma%>%
         filter(ano_lei >=  min(input$ano_loa) & ano_lei <=  max(input$ano_loa),
                ano_despesa >=  min(input$ano_pgt) & ano_despesa <=  max(input$ano_pgt),
-               gnd %in% input$gnd)%>%
+               gnd == "INVESTIMENTOS")%>%
         group_by(ano_lei)%>%
         summarise(pago=sum(valor/1000000000))
       
-      prisma_pago<- prisma%>%
-        group_by(ano_despesa)%>%
-        select(ano_despesa, valor)%>%
-        summarise(pago=sum(valor/1000000000))
+     
       
      
       
@@ -90,12 +82,13 @@ server <- function(input, output) {
           scale_fill_manual(values = "#E69F00")+
           theme_bw()+
           scale_x_continuous(limits=c(2007,2020), breaks  = c(2008, 2009,2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2017, 2018, 2019))+
-          ggtitle("ano do empenho (R$ bi)")+
+          ggtitle("ano do empenho/autorização do investimento (R$ bi)")+
           theme(plot.title=element_text(family='', face='bold', colour='#cc6600', size=11))+
           theme(axis.title.x = element_blank())+
           theme(axis.title.y = element_blank())+
-          theme(axis.text.x = element_text(size = 8)) +
-          geom_text(aes(x=ano_lei, y=pago, label = round(pago)), hjust = 1, vjust = 0, nudge_x = 0.1, nudge_y = 1)+ theme(
+          theme(axis.text.x = element_text(size = 7)) +
+          geom_text(aes(x=ano_lei, y=pago, label = round(pago,2)), size = 2.5,  vjust = -0.5)+ 
+          theme(
             # Remove panel border
             panel.border = element_blank(),  
             # Remove panel grid lines
@@ -125,7 +118,7 @@ server <- function(input, output) {
       prisma_pago<- prisma%>%
         filter(ano_lei >=  min(input$ano_loa) & ano_lei <=  max(input$ano_loa),
                ano_despesa >=  min(input$ano_pgt) & ano_despesa <=  max(input$ano_pgt),
-               gnd %in% input$gnd)%>%
+               gnd == "INVESTIMENTOS")%>%
         group_by(ano_despesa)%>%
         select(ano_despesa, valor)%>%
         summarise(pago=sum(valor/1000000000))
@@ -138,12 +131,13 @@ server <- function(input, output) {
         guides(fill=FALSE)+
         theme_bw()+
         scale_x_continuous(limits=c(2007,2020), breaks  = c(2008, 2009,2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2017, 2018, 2019))+
-        ggtitle("ano do pagamento (R$ bi)")+
+        ggtitle("ano do pagamento do investimento (R$ bi)")+
         theme(plot.title=element_text(family='', face='bold', colour='#ff0000', size=11))+
         theme(axis.title.x = element_blank())+
         theme(axis.title.y = element_blank())+
-        theme(axis.text.x = element_text(size = 8)) + 
-        geom_text(aes(x=ano_despesa, y=pago, label = round(pago)), hjust = 1, vjust = 0, nudge_x = 0.1, nudge_y = 1)+ theme(
+        theme(axis.text.x = element_text(size = 7)) + 
+        geom_text(aes(x=ano_despesa, y=pago, label = round(pago,2)), size = 2.5 ,  vjust = -0.5)+ 
+        theme(
           # Remove panel border
           panel.border = element_blank(),  
           # Remove panel grid lines
